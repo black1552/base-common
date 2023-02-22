f8a046ca948a2efdc6ff1e52b6f01ff846f6735c
清风￡流云 <black1552@163.com>
2023-02-22 19:45:34 +0800

    添加部分方法

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/gogf/gf/v2 v2.1.1
-	gopkg.in/yaml.v2 v2.4.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 )
 
 require (
---
+++ utils.go
@@ -0,0 +1,48 @@
+package v2
+
+import (
+	"github.com/gogf/gf/v2/container/garray"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/nfnt/resize"
+	"image/jpeg"
+	"os"
+)
+
+func Compress(filePath string) string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err.Error())
+	}
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = file.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+	m := resize.Resize(960, 0, img, resize.Lanczos2)
+	str := gstr.Split(filePath, "/")
+	sta := gstr.Split(str[len(str)-1], ".")
+	paths := gfile.Pwd() + "/resource/public/upload/" + sta[0] + "-cop." + sta[1]
+	out, err := os.Create(paths)
+	defer out.Close()
+	err = jpeg.Encode(out, m, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	_ = gfile.Remove(filePath)
+	return sta[0] + "-cop." + sta[1]
+}
+
+func inStrArray(ext string, code int) bool {
+	if code == 1 {
+		arr := garray.NewStrArrayFrom(g.SliceStr{".jpg", ".jpeg", ".png"})
+		return arr.Contains(ext)
+	} else {
+		arr := garray.NewStrArrayFrom(g.SliceStr{".xlsx"})
+		return arr.Contains(ext)
+	}
+}
