bb2261785329ecd849cd372950adf843466a63ef
清风￡流云 <black1552@163.com>
2023-04-12 15:10:01 +0800

    修改日志输出

--- utils.go
+++ utils.go
@@ -1,9 +1,12 @@
 package v2
 
 import (
+	"context"
 	"github.com/gogf/gf/v2/container/garray"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/os/glog"
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/gogf/gf/v2/text/gstr"
 	"github.com/gogf/gf/v2/util/gconv"
@@ -54,7 +57,36 @@ var (
 	Name     = "info_"
 	Exp      = ".log"
 	FilePath = ""
+	log      *glog.Logger
+	ctx      context.Context
 )
+
+func InitLog(path string) {
+	ctx = gctx.New()
+	log = glog.New()
+	logPath := gfile.Pwd() + "/" + path
+	if !gfile.IsDir(logPath) {
+		err := gfile.Mkdir(logPath)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	log.SetStack(true)
+	log.SetStdoutPrint(true)
+	log.SetFile("{Y-m-d}.log")
+	_ = log.SetLevelStr("INFO")
+	_ = log.SetPath(logPath)
+}
+
+func LogInfo(text ...interface{}) {
+	log.Info(ctx, text...)
+}
+func LogError(text ...interface{}) {
+	log.Error(ctx, text...)
+}
+func LogDebug(text ...interface{}) {
+	log.Debug(ctx, text...)
+}
 
 func DeBug(content string) {
 	time := gtime.Datetime()
